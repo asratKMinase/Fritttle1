77614d695778ac58603c25d19ccd09872b4072c2
Asrat Minase <asrat461@revature.net>
2022-06-16 15:41:50 -0500

    Updated the order servlet put method

---
+++ src/main/java/com/revature/frittte/order/OrderInit.java
@@ -0,0 +1,26 @@
+package com.revature.frittte.order;
+
+import com.revature.frittte.customer.Customer;
+import com.revature.frittte.food.Food;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderInit {
+
+
+    private int id;
+    private String orderDate;
+    private String itemName;
+    private String comment;
+    private String customerUsername;
+
+}
--- src/main/java/com/revature/frittte/order/OrderServlet.java
+++ src/main/java/com/revature/frittte/order/OrderServlet.java
@@ -1,10 +1,12 @@
 package com.revature.frittte.order;
 
 
+import com.revature.frittte.creditcard.CreditCard;
 import com.revature.frittte.customer.Customer;
 import com.revature.frittte.customer.CustomerService;
 import com.revature.frittte.food.Food;
 import com.revature.frittte.food.FoodService;
+import com.revature.frittte.util.web.dto.CCInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,17 +44,30 @@ public class OrderServlet {
         newOrder.setCustomer_username(customerService.readById(initOrder.getCustomerUsername()));
 
 
-
         OrderData persistedOrder = orderServices.create(newOrder);
 
         return new ResponseEntity<>(persistedOrder, HttpStatus.CREATED);
 
     }
     @PutMapping("/updateOrder")
-    public ResponseEntity<OrderData> updateCustomer(@RequestBody OrderData order) {
-        OrderData newOrder = orderServices.update(order);
-        return new ResponseEntity<>(newOrder, HttpStatus.OK);
+    public ResponseEntity<OrderData> updateCustomer(@RequestBody OrderInit newOrder ,HttpSession req ) {
+//        OrderData newOrder = orderServices.update(order);
+//        return new ResponseEntity<>(newOrder, HttpStatus.OK);
+        OrderData newOrderData = new OrderData();
+
+        newOrderData.setId(newOrder.getId());
+        newOrderData.setOrderDate(newOrder.getOrderDate());
+        newOrderData.setItemName(foodServices.readById(newOrder.getItemName()));
+        newOrderData.setComment(newOrder.getComment());
+        newOrderData.setCustomer_username(customerService.readById(newOrder.getCustomerUsername()));
+
+        OrderData orderData = orderServices.update(newOrderData);
+
+        return new ResponseEntity<>(orderData, HttpStatus.OK);
+
     }
+
+
     @GetMapping("/findAllMyOrders/{username}")
     public ResponseEntity<List> findAllMyOrders(@PathVariable String username) {
         return new ResponseEntity<>(orderServices.findAllMyOrders(username), HttpStatus.OK);
